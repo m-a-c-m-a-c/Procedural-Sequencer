33a7da600b353b06b3024cfa744fe260bd415342
M.A.C <164536851+m-a-c-m-a-c@users.noreply.github.com>
2024-05-08 14:40:44 -0400

    Add files via upload

---
+++ mySplash.js
@@ -0,0 +1,47 @@
+class Splash {
+
+ constructor() {
+   
+  this.splashBorder = 100;
+  fill(255);
+  stroke(255, 0, 0)
+  rect(this.splashBorder, this.splashBorder, windowWidth-this.splashBorder*2, windowHeight-this.splashBorder*2);
+  fill(0, 0, 222);
+  strokeWeight(3)
+   
+  line(windowWidth-this.splashBorder-40, this.splashBorder+20,windowWidth-this.splashBorder-20, this.splashBorder+40)
+   line(windowWidth-this.splashBorder-20, this.splashBorder+20,windowWidth-this.splashBorder-40, this.splashBorder+40)
+   
+  this.title = createDiv("Procedural Song Sequencer");
+  this.title.style('color:deeppink');
+  this.title.style('font-family: Arial, Helvetica, sans-serif');
+  this.title.position(this.splashBorder+20, this.splashBorder+20);
+  
+  this.name = createDiv("Michael Chase");
+  this.name.position(this.splashBorder+20, this.splashBorder+60);
+  
+  this.info = createDiv("This project is based on the music generation system in the 'cave' levels of the video game, Pikmin 2. That system is based on the random sequencing of pre-written melodies on various sounds to create a procedurally generated composition that still maintains a sense of cohesion. <p> Move the mous left and right to adjust the tempo. <p> Move the mouse up and down to add effects. <p> <p> <a href='https://editor.p5js.org/machase4/sketches/qA3zq25ny'>Code</a>");
+  
+  this.info.position(this.splashBorder+20, this.splashBorder+100);
+  this.info.size(windowWidth-this.splashBorder*2-50, windowHeight-this.splashBorder*2-50)
+   
+
+  
+}
+  
+  update(){
+       if(mouseX > windowWidth-this.splashBorder-40 && 
+          mouseX < windowWidth-this.splashBorder-20 
+          && mouseY < this.splashBorder+40 
+          && mouseY > this.splashBorder+20
+     ){
+     return true
+   }
+  }
+ 
+  hide(){
+    this.title.remove()
+    this.name.remove()
+    this.info.remove()
+  }
+}
